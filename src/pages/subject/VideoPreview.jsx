939d08b41ec98d60fce25696b386620e710d65a2
I Gusti Kade Sugiantara <sugiantara.igk@gmail.com>
2022-05-03 20:26:48 +0700

    add breadcrumbs in video preview component

--- src/pages/subject/VideoPreview.jsx
+++ src/pages/subject/VideoPreview.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
 import Youtube from "react-youtube";
-import { Typography } from "@mui/material";
+import { Box, Typography, Breadcrumbs, Link } from "@mui/material";
 
 const VideoPreview = () => {
   const history = useHistory();
   const content = history.location.state.content;
+  const levelId = history.location.state.levelId;
+  const contentId = history.location.state.contentId;
 
   const opts = {
     height: "500",
@@ -23,6 +25,44 @@ const VideoPreview = () => {
 
   return (
     <div>
+      <Box my={2}>
+        <Breadcrumbs aria-label="breadcrumb">
+          <Link
+            color="inherit"
+            href="/"
+            onClick={(e) => {
+              e.preventDefault();
+              history.push("/subject");
+            }}
+          >
+            Subject
+          </Link>
+          <Link
+            color="inherit"
+            href="/"
+            onClick={(e) => {
+              e.preventDefault();
+              history.push("/content", { id: levelId });
+            }}
+          >
+            Level
+          </Link>
+          <Link
+            color="inherit"
+            href="/"
+            onClick={(e) => {
+              e.preventDefault();
+              history.push("/content-details", {
+                levelId: levelId,
+                contentId: contentId,
+              });
+            }}
+          >
+            Contents
+          </Link>
+          <Typography color="textPrimary">Video Preview</Typography>
+        </Breadcrumbs>
+      </Box>
       <Typography gutterBottom variant="h5" component="div">
         {content.title}
       </Typography>
